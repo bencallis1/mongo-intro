575b9ca37cc41928039262066ae0982e697ab4c1
bencallis1 <bencallis1@gmail.com>
2016-07-07 14:51:04 -0600

    update to the models

--- mongoose/server/api/user/userModel.js
+++ mongoose/server/api/user/userModel.js
@@ -1,12 +1,1 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: true
-  }
-});
-
-module.exports = mongoose.model('user', UserSchema);
+// Create a user schema
